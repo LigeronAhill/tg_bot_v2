2e83339eafbb0219f0b6d1019aee0a608ead9479
Aleksandr <provotorov@inbox.ru>
2023-12-06 18:14:06 +0300

    xl 7

--- src/models/telegram/update/xl.rs
+++ src/models/telegram/update/xl.rs
@@ -1,6 +1,6 @@
 use std::io::Cursor;
 
-use calamine::{open_workbook_from_rs, Reader, Xlsx};
+use calamine::{open_workbook_from_rs, RangeDeserializerBuilder, Reader, Xlsx};
 use serde::{Deserialize, Serialize};
 
 use crate::models::AppState;
@@ -13,51 +13,27 @@ pub async fn stock_update(state: &AppState, uri: &str, name: &str) -> anyhow::Re
     let sheets = workbook.sheet_names().to_owned();
     let range = workbook.worksheet_range(&sheets[0]).unwrap()?;
     let mut result = vec![];
-    for (i, row) in range.rows().enumerate() {
-        if i == 0 {
-            continue;
-        }
-        let Some(brand) = row[0].get_string() else {
-            continue;
-        };
-        let Some(collection) = row[1].get_string() else {
-            continue;
-        };
-        let Some(article) = row[2].get_string() else {
-            continue;
-        };
-        let Some(width) = row[3].get_int() else {
-            continue;
-        };
-        let Some(free_m) = row[4].get_float() else {
-            continue;
-        };
-        let Some(free_sqm) = row[5].get_float() else {
-            continue;
-        };
-        result.push(Carpetland {
-            brand: brand.to_string(),
-            collection: collection.to_string(),
-            article: article.to_string(),
-            width: width as i32,
-            free_m,
-            free_sqm,
-        })
-    }
+    let iter = RangeDeserializerBuilder::new().from_range::<_, RawCarpetland>(&range)?;
     let test = match name.contains("Carpetland") {
         true => "passed",
         false => "failed",
     };
+    for row in iter {
+        match row {
+            Ok(row) => result.push(row),
+            Err(_) => continue,
+        }
+    }
     let text = format!("file: {}\ntest: {}\n{:#?}", name, test, result[2]);
     state.bot.send_message_admin(&text).await?;
     Ok(())
 }
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Carpetland {
+pub struct RawCarpetland {
     pub brand: String,
     pub collection: String,
     pub article: String,
-    pub width: i32,
-    pub free_m: f64,
-    pub free_sqm: f64,
+    pub width: String,
+    pub free_m: String,
+    pub free_sqm: String,
 }
